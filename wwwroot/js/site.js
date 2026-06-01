98504aecd7b483e676978bb2309a57acab15a775
hiko667 <kuleszastanislaw7547@gmail.com>
2026-06-01 18:02:29 +0200

    Some css

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,4 +1,37 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿document.addEventListener("DOMContentLoaded", function () {
+    const htmlElement = document.getElementById("theme-html");
+    const themeToggleBtn = document.getElementById("theme-toggle");
+    console.log("ninja")
+    const savedTheme = localStorage.getItem("theme");
 
-// Write your JavaScript code.
+    if (savedTheme) {
+        htmlElement.setAttribute("data-bs-theme", savedTheme);
+        updateButtonText(savedTheme);
+    }
+
+    themeToggleBtn.addEventListener("click", function () {
+        console.log("Ninja")
+        const currentTheme = htmlElement.getAttribute("data-bs-theme");
+        let newTheme = "dark";
+
+        if (currentTheme === "dark") {
+            newTheme = "light";
+        }
+
+        htmlElement.setAttribute("data-bs-theme", newTheme);
+        localStorage.setItem("theme", newTheme);
+        updateButtonText(newTheme);
+    });
+
+    function updateButtonText(theme) {
+        if (theme === "dark") {
+            themeToggleBtn.textContent = "Light Mode";
+            themeToggleBtn.className = "btn btn-light btn-sm";
+        } else {
+            themeToggleBtn.textContent = "Dark Mode";
+            themeToggleBtn.className = "btn btn-dark btn-sm ";
+        }
+    }
+
+    updateButtonText(htmlElement.getAttribute("data-bs-theme"));
+});
