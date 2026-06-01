5a1077011477c2604776990ee6e7a5922c33940f
hiko667 <kuleszastanislaw7547@gmail.com>
2026-06-01 19:42:32 +0200

    css fixes

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,37 +1,47 @@
-﻿document.addEventListener("DOMContentLoaded", function () {
-    const htmlElement = document.getElementById("theme-html");
-    const themeToggleBtn = document.getElementById("theme-toggle");
+﻿// const savedTheme = localStorage.getItem("theme");
+// const theme = document.getElementById("theme-html").attributes.getAttribute("data-bs-theme")
+
+// if (savedTheme && savedTheme !== theme) {
+//     document.body.classList.add("no-transition");
+//     htmlElement.setAttribute("data-bs-theme", savedTheme);
+//     document.body.classList.remove("no-transition");
+// }
+
+document.addEventListener("DOMContentLoaded", function () {
+    const htmlElement = document.getElementById("theme-html")
+    const themeToggleBtn = document.getElementById("theme-toggle")
+    const themeIcon = document.getElementById("theme-icon")
     console.log("ninja")
-    const savedTheme = localStorage.getItem("theme");
+    const savedTheme = localStorage.getItem("theme")
 
     if (savedTheme) {
-        htmlElement.setAttribute("data-bs-theme", savedTheme);
-        updateButtonText(savedTheme);
+        htmlElement.setAttribute("data-bs-theme", savedTheme)
+        updateButtonIcon(savedTheme)
     }
-
+    
     themeToggleBtn.addEventListener("click", function () {
         console.log("Ninja")
-        const currentTheme = htmlElement.getAttribute("data-bs-theme");
-        let newTheme = "dark";
+        const currentTheme = htmlElement.getAttribute("data-bs-theme")
+        let newTheme = "dark"
 
         if (currentTheme === "dark") {
-            newTheme = "light";
+            newTheme = "light"
         }
 
-        htmlElement.setAttribute("data-bs-theme", newTheme);
-        localStorage.setItem("theme", newTheme);
-        updateButtonText(newTheme);
+        htmlElement.setAttribute("data-bs-theme", newTheme)
+        localStorage.setItem("theme", newTheme)
+        updateButtonIcon(newTheme)
     });
 
-    function updateButtonText(theme) {
+    function updateButtonIcon(theme) {
         if (theme === "dark") {
-            themeToggleBtn.textContent = "Light Mode";
-            themeToggleBtn.className = "btn btn-light btn-sm";
+            themeIcon.className = "bi bi-sun-fill"
+            themeToggleBtn.className = "btn btn-lg"
         } else {
-            themeToggleBtn.textContent = "Dark Mode";
-            themeToggleBtn.className = "btn btn-dark btn-sm ";
+            themeIcon.className = "bi bi-moon-fill"
+            themeToggleBtn.className = "btn btn-lg"
         }
     }
 
-    updateButtonText(htmlElement.getAttribute("data-bs-theme"));
+    updateButtonText(htmlElement.getAttribute("data-bs-theme"))
 });
